49fd83d3a0f72abfcd0080df090976a3c484ec9d
SSNmeister <110984640+SSNmeister@users.noreply.github.com>
2022-10-13 09:44:35 +0800

    Initial commit with home page, navbar, header. + modal components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import React, { useState } from "react";
-import ModalHowToApplyForItems from "./components/ModalHowToApplyForItems";
-import Header from "./Header";
-import NavBar from "./navbar/NavBar";
+import React from "react";
+
 import Home from "./pages/Home";
-import ReactApp from "./components/ReactApp";
 
 function App() {
-  const [openModalApplyItems, setOpenModalApplyItems] = useState(false);
-
-  return <ReactApp />;
-  // return (
-  // <div className="main-app-container">
-  //   {!openModalApplyItems ? (
-  //     <>
-  //       <div className="main-header-container">
-  //         <Header />
-  //       </div>
-  //       <div className="main-body-container">
-  //         <Home setOpenModalApplyItems={setOpenModalApplyItems} />
-  //       </div>
-  //       <div className="main-navbar">
-  //         <NavBar />
-  //       </div>
-  //     </>
-  //   ) : (
-  //     <div className="main-modal-applyforitems-container">
-  //       <ModalHowToApplyForItems
-  //         setOpenModalApplyItems={setOpenModalApplyItems}
-  //       />
-  //     </div>
-  //   )}
-  // </div>
-  // );
+  return (
+    <div className="main-app-container">
+      <>
+        <div className="main-body-container">
+          <Home />
+        </div>
+      </>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,70 +1,95 @@
-import React from "react";
-import { UilUserPlus } from "@iconscout/react-unicons";
-import { UilCalendarAlt } from "@iconscout/react-unicons";
-import { UilBookOpen } from "@iconscout/react-unicons";
-import { UilMedkit } from "@iconscout/react-unicons";
-
+import React, { useState } from "react";
+import Header from "../Header";
 import NavBar from "../navbar/NavBar";
 
-const Home = ({ setOpenModalApplyItems }) => {
-  return (
-    <div className="home-container">
-      <div className="home-top-section">
-        <h1>Hi, Desmond</h1>
-        <div className="item-application-container">
-          <div
-            className="item-application-box"
-            onClick={() => {
-              setOpenModalApplyItems(true);
-            }}
-          >
-            <UilUserPlus className="item-application-icon" />
-          </div>
-          <p>Item Application</p>
-        </div>
-      </div>
-      <div className="home-bottom-section">
-        <div className="activity-individual-boxes">
-          <div className="activity-icon-box">
-            <UilCalendarAlt className="activity-icon" />
-          </div>
-          <p>Events</p>
-        </div>
-        <div className="activity-individual-boxes">
-          <div className="activity-icon-box">
-            <UilBookOpen className="activity-icon" />
-          </div>
-          <p>Lessons</p>
-        </div>
-        <div className="activity-individual-boxes">
-          <div className="activity-icon-box">
-            <UilMedkit className="activity-icon" />
-          </div>
-          <p>Medical</p>
-        </div>
-      </div>
+import donate from "../assets/donate.png";
+import education from "../assets/education.png";
+import love from "../assets/love.png";
+import corporatepartnership from "../assets/corporatepartnership.png";
+import ModalDonateInstructions from "../components/ModalDonateInstructions";
+import ModalHowToDonateForItems from "../components/ModalHowToDonateForItems";
 
-      {/* <div className="navbar-container">
-        <div className="navbar-individual-boxes">
-          <div className="navbar-icon-box">
-            <UilEstate className="navbar-icon" />
-          </div>
-          <p className="navbar-fonts">Home</p>
+const Home = () => {
+  const [openModalDonateInstructions, setopenModalDonateInstructions] =
+    useState(false);
+  const [openModalApplyItems, setOpenModalApplyItems] = useState(false);
+
+  return (
+    <>
+      <div className="main-home-container">
+        <div className="main-header-container">
+          <Header />
         </div>
-        <div className="navbar-individual-boxes">
-          <div className="navbar-icon-box">
-            <UilClipboardAlt className="navbar-icon" />
+        <div className="home-container">
+          <div className="home-top-section">
+            <h1 className="home-h1-22">Hi, Sophie</h1>
+            <p className="home-h2-14">How can you help?</p>
+            <div className="item-application-container">
+              <div
+                className="item-application-box"
+                onClick={() => {
+                  setopenModalDonateInstructions(true);
+                  console.log(openModalApplyItems);
+                }}
+              >
+                <img
+                  src={donate}
+                  alt="donateimage"
+                  className="donate-application-icon"
+                />
+              </div>
+              <p className="noto-12">Donate</p>
+            </div>
           </div>
-          <p className="navbar-fonts">Application</p>
-        </div>
-        <div className="navbar-individual-boxes">
-          <div className="navbar-icon-box">
-            <UilEllipsisH className="navbar-icon" />
+          <div className="home-bottom-section">
+            <div className="activity-individual-boxes">
+              <div className="activity-icon-box">
+                <img
+                  src={love}
+                  alt="loveimage"
+                  className="item-application-icon"
+                />
+              </div>
+              <p className="noto-12">Volunteer at IRR</p>
+            </div>
+            <div className="activity-individual-boxes">
+              <div className="activity-icon-box">
+                <img
+                  src={corporatepartnership}
+                  alt="corporatepartnershipimage"
+                  className="item-application-icon"
+                />
+              </div>
+              <p className="noto-12">Corporate Partnership</p>
+            </div>
+            <div className="activity-individual-boxes">
+              <div className="activity-icon-box">
+                <img
+                  src={education}
+                  alt="educationimage"
+                  className="item-application-icon"
+                />
+              </div>
+              <p className="noto-12">Youth & Education</p>
+            </div>
           </div>
-          <p className="navbar-fonts">More</p>
         </div>
-      </div> */}
-    </div>
+        <div className="main-navbar">
+          <NavBar />
+        </div>
+      </div>
+      <ModalDonateInstructions
+        openModalDonateInstructions={openModalDonateInstructions}
+        setopenModalDonateInstructions={setopenModalDonateInstructions}
+      />
+      <div className="main-modal-applyforitems-container">
+        <ModalHowToDonateForItems
+          setOpenModalApplyItems={setOpenModalApplyItems}
+          openModalApplyItems={openModalApplyItems}
+          className="main-modal-applyforitems-container"
+        />
+      </div>
+    </>
   );
 };
 
