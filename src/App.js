07a7ba84ba2d6e432f506327693540a607842e9a
Andrew Tai <andrewtai.ye@gmail.com>
2022-10-13 11:11:58 +0800

    Changed to email and key png to svg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Home from "./pages/Home";
+import ReactApp from "./components/ReactApp";
 
 function App() {
   return (
     <div className="main-app-container">
       <>
         <div className="main-body-container">
-          <Home />
+          <ReactApp />
         </div>
       </>
     </div>
