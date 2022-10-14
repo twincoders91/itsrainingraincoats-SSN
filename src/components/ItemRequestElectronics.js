ce78094ecaf20cfb22611b96c2599a423931c603
Andrew Tai <andrewtai.ye@gmail.com>
2022-10-14 23:25:56 +0800

    Added ItemRequestAdd and ItemRequestElectronics page

---
+++ src/components/ItemRequestElectronics.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+import plug from "../assets/plug.svg";
+import personalcare from "../assets/personalcare.svg";
+import clothes from "../assets/clothes.svg";
+import mobilephone from "../assets/mobilephone.svg";
+import microwave from "../assets/microwave.svg";
+import bicycle from "../assets/bicycle.svg";
+import uploadpicture from "../assets/uploadpicture.svg";
+
+export default function ItemRequest1() {
+  return (
+    <div className="col">
+      <span className="request-subsubtitleText request-header mb-2">
+        Step 1 of 3 - Select item(s)
+      </span>
+      <span className="request-titleText request-header mb-4">
+        What item(s) would you like to request?
+      </span>
+      <div className="grid gc-3 mb-4">
+        <div className="col">
+          <button className="request-icon-button mb-1">
+            <img src={plug} alt="" />
+          </button>
+          <span className="request-subsubtitleText request-icon-text fw-600">
+            Food & Drinks
+          </span>
+        </div>
+        <div className="col">
+          <button className="request-icon-button mb-1">
+            <img src={personalcare} alt="" />
+          </button>
+          <span className="request-subsubtitleText request-icon-text fw-600">
+            Personal Care
+          </span>
+        </div>
+        <div className="col">
+          <button className="request-icon-button mb-1">
+            <img src={clothes} alt="" />
+          </button>
+          <span className="request-subsubtitleText request-icon-text fw-600">
+            Clothing
+          </span>
+        </div>
+        <div className="col">
+          <button className="request-icon-button mb-1">
+            <img src={mobilephone} alt="" />
+          </button>
+          <span className="request-subsubtitleText request-icon-text fw-600">
+            Electronics
+          </span>
+        </div>
+        <div className="col">
+          <button className="request-icon-button mb-1">
+            <img src={microwave} alt="" />
+          </button>
+          <span className="request-subsubtitleText request-icon-text fw-600">
+            Appliances
+          </span>
+        </div>
+        <div className="col">
+          <button className="request-icon-button mb-1">
+            <img src={bicycle} alt="" />
+          </button>
+          <span className="request-subsubtitleText request-icon-text fw-600">
+            Sports & Games
+          </span>
+        </div>
+      </div>
+      <span className="request-titleText request-header mb-2">
+        Item not found?
+      </span>
+      <input
+        className="mb-2"
+        type="text"
+        placeholder="Enter name of the item"
+      />
+      <span className="request-subtitleText mb-2">or</span>
+      <input className="hidden" id="request-picture-upload" type="file"></input>
+      <label
+        className="request-upload row mb-4"
+        htmlFor="request-picture-upload"
+      >
+        <img src={uploadpicture} alt="" />
+        <span className="request-titleText fw-600 ml-1">Add Photo</span>
+      </label>
+      <div className="row">
+        <svg width="10px" height="10px" className="mr-2">
+          <circle cx="5" cy="5" r="5" fill="rgb(var(--blue))" />
+        </svg>
+        <svg width="10px" height="10px">
+          <circle cx="5" cy="5" r="5" fill="rgb(var(--grey1))" />
+        </svg>
+        <svg width="10px" height="10px" className="ml-2">
+          <circle cx="5" cy="5" r="5" fill="rgb(var(--grey1))" />
+        </svg>
+      </div>
+    </div>
+  );
+}
--- src/pages/ItemRequest.js
+++ src/pages/ItemRequest.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import ItemRequest1 from "../components/ItemRequest1";
+import ItemRequestAdd from "../components/ItemRequestAdd";
 
 import Header from "../Header";
 
 export default function ItemRequest() {
-  const [currentPage, setCurrentPage] = useState("request1");
+  const [currentPage, setCurrentPage] = useState("addItem");
+
   return (
     <>
       <div className="main-header-container">
         <Header />
       </div>
       <div className="request-body-container">
-        <ItemRequest1 />
+        <ItemRequestAdd />
       </div>
     </>
   );
