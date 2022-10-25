47001b798750633d535aa2d5920e12a7148de1c9
Andrew Tai <andrewtai.ye@gmail.com>
2022-10-25 22:09:20 +0800

    Finished Worker Application Page

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -6,9 +6,11 @@ import HeaderGeneric from "../components/HeaderGeneric";
 import NavBar from "../navbar/NavBar";
 
 import { capitaliseFirstLetter } from "../components/utility";
+import WorkerApplicationItem from "../components/WorkerApplicationItem";
 
 const Application = () => {
   const [displayArray, setDisplayArray] = useState([]);
+  const [requestCount, setRequestCount] = useState("");
   const context = useContext(Context);
 
   const getItemRequests = async (url) => {
@@ -26,7 +28,8 @@ const Application = () => {
       const url = `http://127.0.0.1:5001/item_request/${context.userId}`;
       const res = await getItemRequests(url);
       console.log(res);
-      setDisplayArray(res);
+      setDisplayArray(res.data);
+      setRequestCount(res.count);
     })();
   }, []);
 
@@ -34,27 +37,15 @@ const Application = () => {
     <div style={{ position: "relative", height: "100%" }}>
       <HeaderGeneric headerTitle="Applications" profile={true} />
       <div className="px-2 py-2">
-        <span className="fs-12 fw-300 mb-2">Applied Item(s): {`count`}</span>
+        <p className="fs-12 fw-300 mb-2">Applied Item(s): {requestCount}</p>
         {displayArray.map((element, index) => {
           return (
-            <div key={index} className="application__card-container">
-              <div className="row mb-2 px-2 py-1" style={{ width: "100%" }}>
-                {/* <img
-                  src={require(`../assets/${element.icon}`)}
-                  alt=""
-                  height="32px"
-                /> */}
-                Icon
-                <div
-                  className="col ml-2"
-                  style={{ flex: 1, alignItems: "flex-start" }}
-                >
-                  {/* Item Name */} <p>{capitaliseFirstLetter(element.item)}</p>
-                  {/* Waiting Time */} <p>Time</p>
-                </div>
-                {/* Delete Button Here */} Delete
-              </div>
-            </div>
+            <WorkerApplicationItem
+              {...element}
+              key={index}
+              index={index}
+              setDisplayArray={setDisplayArray}
+            />
           );
         })}
       </div>
