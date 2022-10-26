bd6f3ec02d0a229b0844d85d15cdfe4d9724675f
Azheem Rahman <azheem.rahman@gmail.com>
2022-10-26 14:19:48 +0800

    added logout function to worker and donor homepages

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import union from "./assets/union.svg";
 import irrlogo from "./assets/irrlogo.svg";
+import Context from "./context/context";
 
 const Header = () => {
   // should header have 2 views, generic and top navigation?
+
+  const context = useContext(Context);
+
+  // logout function: when user clicks button to logout, clear all localStorage and setLogOut true
+  const logout = () => {
+    localStorage.clear();
+    context.setLogOut(true);
+  };
 
   return (
     <div className="Navbar">
@@ -11,7 +20,12 @@ const Header = () => {
         <img className="nav-logo-image" src={irrlogo} alt="raincoatlogo" />
       </div>
       <div className="nav-left-item">
-        <img src={union} className="nav-user-icon" alt="user-profile" />
+        <img
+          src={union}
+          className="nav-user-icon"
+          alt="user-profile"
+          onClick={logout}
+        />
       </div>
     </div>
   );
